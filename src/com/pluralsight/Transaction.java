634e2a5cfe4f94edbc53da75c3b21f4e45d20875
MohammedKhan <mohammedk20259@gmail.com>
2025-10-14 14:41:24 -0400

    Added constructor to the transaction class.

--- src/com/pluralsight/Transaction.java
+++ src/com/pluralsight/Transaction.java
@@ -10,9 +10,20 @@ public class Transaction {
     private LocalDate date; // the date transaction happended
     private LocalTime time; // the time the transaction happened.
     private String description; // this tells what the transaction was for.
-    private String vendor; // who the the transaction happened with.
+    private String vendor; // who the transaction happened with.
     private double amount; // The dollar amount.
     // it should come out positive for deposits and negative for payments.
-    
+
+    // constructor - this method runs everytime a new transaction created
+    // it takes all the information and puts them into to private labels.
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        // side note this. - means its refering to the current object's variables.
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
 
 }
